07919f105313fd7cca769baa1faa9ed8859787f5
cahyanatea003 <cahyanatea003@gmail.com>
2019-10-30 14:20:18 +0700

    Custom Login Form

---
+++ src/main/java/com/example/demo/oauth2/config/KonfigurasiWeb.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.demo.oauth2.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ *
+ * @author okt19
+ */
+@Configuration
+public class KonfigurasiWeb extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/materi/list").setViewName("/materi/list");
+    }
+
+}
